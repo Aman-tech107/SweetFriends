8345fb717d65d70badf173b1b8657f453bc5f026
unknown <you@example.com>
2020-06-28 14:57:09 +0530

    Friends

--- sketch.js
+++ sketch.js
@@ -21,13 +21,13 @@ function setup(){
     engine = Engine.create();
     world = engine.world;
 
-
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
+    pig1.image = loadImage("sprites/Friend1.png");
     log1 = new Log(810,260,300, PI/2);
 
     box3 = new Box(700,240,70,70);
